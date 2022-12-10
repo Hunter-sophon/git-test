d2db1ff59dda2ba637be263a716fff60ab830e20
WuJunLing <1796525087@qq.com>
2022-12-10 22:50:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author JunLing Wu
+ * @create 2022-12-10 22:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
